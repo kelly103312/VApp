e3a9a8ad55c4f78bebe60b3ba4fdc41c0aeb2e9e
kelly103312 <kvquintana@REDCOLSA.LOCAL>
2024-07-03 10:24:24 -0500

    Form v0.1

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,29 @@
 <template>
-    <div>
-      <button class="btn btn-success">Ingresar</button>
+    <div class="container">
+      <form class="" @submit.prevent="submit">
+        <div class="form-group">
+          <label class="form-label"> Usuario </label>
+          <input class="form-control" type="text">
+        </div>
+        <div class="form-group">
+          <label class="form-label"> Contraseña </label>
+          <input class="form-control" type="password">
+        </div>
+        <div class="mt-4">
+          <button type="submit" class="btn btn-success">Ingresar</button>
+        </div>
+      </form>
     </div>
   </template>
   
   <script>
   export default {
-    name: 'LoginComponent'
+    name: 'LoginComponent',
+    methods: {
+      submit() {
+        console.log("Holi")
+      }
+    }
   }
   </script>
   
