c409e92bd88de938d37129ef292da6208b9ef2af
kelly103312 <kvquintana@REDCOLSA.LOCAL>
2024-07-03 09:38:55 -0500

    Inicio app

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <button class="btn btn-success">Ingresar</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LoginComponent'
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+ 
+  </style>
+  
